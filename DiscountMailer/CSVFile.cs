0ea4c1f3506d266022fde45b4b64b20df8a6ab2d
angulardev23 <garnoslaw94@gmail.com>
2018-09-29 14:18:47 +0200

    init project

---
+++ DiscountMailer/CSVFile.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace DiscountMailer
+{
+    class CSVFile
+    {
+        public static void ReadCSV()
+        {
+            using (var reader = new StreamReader(@"C:\Users\garu\Desktop\test.csv"))
+            {
+                List<string> listA = new List<string>();
+                List<string> listB = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    Console.WriteLine(values[0]);
+                    listA.Add(values[0]);
+                    //listB.Add(values[1]);
+                }
+            }
+        }
+    }
+}
---
+++ DiscountMailer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DiscountMailer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Settings settings = new Settings();
+            CSVFile.ReadCSV();
+            //settings.ReadSettings();
+            //Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
