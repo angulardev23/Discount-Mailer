331646001503f493cb20003c3223e661b99eaac9
garu <garnoslaw94@gmail.com>
2018-10-06 21:27:14 +0200

    update main program and add new properties to appsettings.json

--- Startup/Program.cs
+++ Startup/Program.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using DiscountMailer;
+using System;
 
 namespace Startup
 {
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {   //read and create settings file
             Settings settings = new Settings();
+            //start sending
+            EmailSender emailSender = new EmailSender();
+            emailSender.Sending();
         }
     }
 }
