92c267e6333c87545736c216f6ec6286edc534bc
garu <garnoslaw94@gmail.com>
2018-10-10 23:37:17 +0200

    add email class

---
+++ Email/EmailRecipient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Email
+{
+    public class EmailRecipient
+    {
+        public string EmailAddress { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime EndDateTime { get; set; }
+
+        EmailRecipient(string emailAddress, string name, string surname, DateTime endDateTime)
+        {
+            EmailAddress = emailAddress;
+            Name = name;
+            Surname = surname;
+            EndDateTime = endDateTime;
+        }
+    }
+}
